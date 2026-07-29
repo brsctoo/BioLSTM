cd365be2e0c4eaa62c302737bbb23e96cb845ca4
brsctoo <bcominscheffel@gmail.com>
2026-07-29 09:23:56 -0300

    fix: EarlyStopping

--- src/train_model.py
+++ src/train_model.py
@@ -64,8 +64,9 @@ def train_model_gene_split(XY_train_filepath, XY_val_filepath, result_filepath_o
 
     # Configuração do Early Stopping rigoroso para o fluxo gene-split (paciência de 3 épocas)
     early_stopping = EarlyStopping(
-        monitor='val_loss',
-        patience=3,
+        monitor='val_auc',
+        mode='max',
+        patience=10,
         restore_best_weights=True,
         verbose=1
     )
